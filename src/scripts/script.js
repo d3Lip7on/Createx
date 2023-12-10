68845107f459ece4928758ee321aa3288f72ab78
YarikAssShot <yarikassshot@gmail.com>
2023-12-10 14:08:56 +0100

    navbar sec commit (Не закончено)

--- src/scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var burgerMenu = document.getElementById('burger__menu');
-    var mobileMenu = document.getElementById('mobile__menu');
-
-    burgerMenu.addEventListener('click', function () {
-        mobileMenu.classList.toggle('show');
-    });
-});
