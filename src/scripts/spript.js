8404aaeaa8ad6877a9ccb1d3ed038911122e2a1a
Nazar <nazarkhrolenko@gmail.com>
2023-12-21 15:53:53 +0100

    footerversion1

---
+++ src/scripts/spript.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){   
+//     $('.title__contacts').click(function(event){
+//        $(this).toggleClass('active').next().slideToggle(300);
+//     });
+// });
+// setTimeout(()=>{},300)
