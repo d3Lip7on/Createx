68c4513d1defa559ca754af61f22bbc8a3fc3fc3
Vladislav Vagner <vladislawwagner@gmail.com>
2024-01-03 02:26:10 +0100

    put icons into folder

--- src/scripts/spript.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function(){   
-//     $('.title__contacts').click(function(event){
-//        $(this).toggleClass('active').next().slideToggle(300);
-//     });
-// });
-// setTimeout(()=>{},300)
