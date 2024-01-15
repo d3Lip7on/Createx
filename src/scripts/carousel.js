1219699742840072a9147011b98207ecd0145eb7
7t3an <chizazez77@gmail.com>
2024-01-16 00:02:51 +0100

    wqqww git commit -m weqwe git commit -m q

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -3,6 +3,11 @@
 // 		type: 'loop',
 // 		padding: '5rem',
 // 	});
+// document.addEventListener('DOMContentLoaded', function () {
+// 	var splide = new Splide('.splide', {
+// 		type: 'loop',
+// 		padding: '5rem',
+// 	});
 
 // 	splide.mount();
 // });
@@ -46,3 +51,36 @@ splide.mount();
 // 	main.mount();
 // 	thumbnails.mount();
 // });
+
+// 	splide.mount();
+// });
+
+// var splide = new Splide( '.splide',{
+// 	// padding: { left: '200px', right: '345px' },
+// 	gap: '1200px',
+// } );
+// splide.mount();
+// var thumbnails = new Splide( '#thumbnail-slider', {
+// 	rewind          : true,
+// 	fixedWidth      : 104,
+// 	fixedHeight     : 58,
+// 	isNavigation    : true,
+// 	gap             : 10,
+// 	focus           : 'center',
+// 	pagination      : false,
+// 	cover           : true,
+// 	dragMinThreshold: {
+// 	  mouse: 4,
+// 	  touch: 10,
+// 	},
+// 	breakpoints : {
+// 	  640: {
+// 		fixedWidth  : 66,
+// 		fixedHeight : 38,
+// 	  },
+// 	},
+//   } );
+  
+//   main.sync( thumbnails );
+//   main.mount();
+//   thumbnails.mount();
