7f7b7cb7a1e6e55a8c734f9cace19eec9a69e1ea
7t3an <chizazez77@gmail.com>
2024-01-16 00:01:14 +0100

    dqdqw

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,10 +1,48 @@
-import Splide from '@splidejs/splide';
+// document.addEventListener('DOMContentLoaded', function () {
+// 	var splide = new Splide('.splide', {
+// 		type: 'loop',
+// 		padding: '5rem',
+// 	});
 
-document.addEventListener('DOMContentLoaded', function () {
-	// var splide = new Splide('.splide', {
-	// 	type: 'fade',
-	// 	rewind: true,
-	// });
+// 	splide.mount();
+// });
 
-	splide.mount();
-});
+var splide = new Splide( '.splide',{	
+	gap: '1200px',
+	speed: number = 1300
+} );
+splide.mount();
+// document.addEventListener('DOMContentLoaded', function () {
+// 	var main = new Splide('#main-slider', {
+// 		type: 'fade',
+// 		heightRatio: 0.5,
+// 		pagination: false,
+// 		arrows: false,
+// 		cover: true,
+// 	});
+
+// 	var thumbnails = new Splide('#thumbnail-slider', {
+// 		rewind: true,
+// 		fixedWidth: 100,
+// 		fixedHeight: 100,
+// 		isNavigation: true,
+// 		gap: 10,
+// 		focus: 'center',
+// 		pagination: false,
+// 		cover: true,
+// 		dragMinThreshold: {
+// 			mouse: 4,
+// 			touch: 10,
+// 		},
+// 		breakpoints: {
+// 			640: {
+// 				fixedWidth: 66,
+// 				fixedHeight: 38,
+// 			},
+// 		},
+// 	});
+
+// 	main.sync(thumbnails);
+// 	main.mount();
+// 	thumbnails.mount();
+// });
