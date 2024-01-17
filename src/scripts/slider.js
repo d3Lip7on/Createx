36d23f56a44d9980b54b83b6642d3dd5402c7c80
Nazar <nazarkhrolenko@gmail.com>
2024-01-17 23:08:52 +0100

    slider not refacted6

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-	$('.slider').slick({
-		arrows: true,
-		dots: false,
-		adaptiveHeight: true,
-		slidesToShow: 3,
-		speed: 1000,
-	});
+  $(".slider").slick({
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    speed: 1000,
+    infinite: false,
+  });
 });
