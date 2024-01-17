8fc2c234b1b1da8437d9594f04dd073a58ccf093
Vladislav Vagner <vladislawwagner@gmail.com>
2024-01-17 16:32:09 +0100

    carousel v_1

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,5 +1,35 @@
-var splide = new Splide( '.splide',{	
-	gap: '1200px',
-	speed: number = 1300
-} );
-splide.mount();
+// var splide = new Splide( '.splide',{
+// 	gap: '1200px',
+// 	speed: number = 1300
+// } );
+// splide.mount();
+
+// document.addEventListener('DOMContentLoaded', function () {
+// 	new Splide('#thumbnail-carousel', {
+// 		fixedWidth: 100,
+// 		gap: 10,
+// 		rewind: true,
+// 		pagination: false,
+// 	}).mount();
+// });
+var main = new Splide('#main-carousel', {
+	type: 'fade',
+	rewind: true,
+	pagination: false,
+	arrows: true,
+});
+
+const thumbnailSlider = new Splide('#thumbnail-carousel', {
+	fixedWidth: 100,
+	fixedHeight: 100,
+	gap: 10,
+	rewind: true,
+	pagination: false,
+	isNavigation: true,
+	arrows: false,
+	gap: 20,
+	drag: false,
+});
+main.sync(thumbnailSlider);
+main.mount();
+thumbnailSlider.mount();
