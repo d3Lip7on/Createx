94df5c53a4b12575954bdaa734b8a6ec000c8d43
Nazar <nazarkhrolenko@gmail.com>
2024-01-20 17:29:37 +0100

    rebase commit

--- src/scripts/service_v2_script.js
+++ src/scripts/service_v2_script.js
@@ -1,14 +1,21 @@
-const offerspoiler = document.querySelector(".WEBpage__offer-subtitle");
-const infospoiler = document.querySelector(".WEBpage__offer-text");
+// const offerspoiler = document.querySelector(".offers__subtitle");
+// const infospoiler = document.querySelector(".WEBpage__offer-text");
 
-offerspoiler.onclick = () => {
-  infospoiler.classList.toggle("active");
-  offerspoiler.classList.toggle("WEBpage__offer-subtitle_active");
-};
-const offerspoiler1 = document.querySelector(".WEBpage__offer-subtitle2");
-const infospoiler1 = document.querySelector(".WEBpage__offer-text2");
+// offerspoiler.onclick = () => {
+//   infospoiler.classList.toggle("active");
+//   offerspoiler.classList.toggle("offers__subtitle_active");
+// };
+// const offerspoiler1 = document.querySelector(".offers__subtitle");
+// const infospoiler1 = document.querySelector(".WEBpage__offer-text2");
 
-offerspoiler1.onclick = () => {
-  infospoiler1.classList.toggle("active");
-  offerspoiler1.classList.toggle("WEBpage__offer-subtitle2_active");
-};
+// offerspoiler1.onclick = () => {
+//   infospoiler1.classList.toggle("active");
+//   offerspoiler1.classList.toggle("offers__subtitle_active");
+// };
+
+const offerspoilers = document.querySelectorAll(".offers__subtitle");
+offerspoilers.forEach((offerspoiler) => {
+  offerspoiler.addEventListener("click", () => {
+    offerspoiler.classList.toggle("offers__subtitle_active");
+  });
+});
