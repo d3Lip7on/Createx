f21f88893db44036daf62de1f4cf412ed90b22c1
YarikAssShot <yarikassshot@gmail.com>
2024-02-27 14:01:05 +0100

    homepage - top-section finished - reworked javascript code for top-section slider - added media request - some changes

--- src/scripts/sliderTopSection.js
+++ src/scripts/sliderTopSection.js
@@ -1,7 +1,7 @@
 let slideIndex = 1;
 
 function slider_arrow(param){
-    const element = document.getElementById("test");
+    const element = document.getElementById("top-section");
     let prevIndex = slideIndex - 1 < 1 ? 4 : slideIndex - 1;
     let nextIndex = slideIndex + 1 > 4 ? 1 : slideIndex + 1;
 
@@ -14,10 +14,22 @@ function slider_arrow(param){
         element.classList.add("top-section_img" + prevIndex)
         slideIndex = prevIndex
     }
+    updateActiveClass();
 }
 function span_button(param){
-    const element = document.getElementById("test");
+    const element = document.getElementById("top-section");
     element.classList.remove("top-section_img" + slideIndex);
     slideIndex=param
     element.classList.add("top-section_img" + slideIndex)
+    updateActiveClass();
+}
+function updateActiveClass() {
+    for (let i = 1; i <= 4; i++) {
+        const spanElement = document.getElementById("slider-block" + i);
+        if (i === slideIndex) {
+            spanElement.classList.add("top-section__block_active");
+        } else {
+            spanElement.classList.remove("top-section__block_active");
+        }
+    }
 }
